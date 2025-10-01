6b69b37dff14d1f33b4bfaf3742a4bf6d7faa3b6
Yahir1820 <zs25018113@estudiantes.uv.mx>
2025-10-01 09:52:33 -0600

    primer commit

---
+++ proyectoscpp/prac1/holas.cpp
@@ -0,0 +1,15 @@
+//Para COMPILAR el programa en cmd usar g++ holas.cpp -o holas
+//Para correrlo solo poner "holas" en cmd
+
+//
+
+#include <iostream>
+using namespace std;
+
+int main () {
+   cout << "Hola Coatzacoalcos, buenos dias"<<endl;
+   cout << "FF";
+
+
+    return 0;
+}
